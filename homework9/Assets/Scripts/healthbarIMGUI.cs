7c896f2b6d6b51cab4458e67f6f756fd8828946f
Shanzhulizhi <1723158024@qq.com>
2019-11-17 11:27:42 +0800

    homework9

---
+++ homework9/Assets/Scripts/healthbarIMGUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbarIMGUI : MonoBehaviour {
+    public Slider slider;
+    public float health = 0.0f; //初始血条
+    public float curhealth;  //结果血条
+    public Rect HealthBar;
+    public Rect ButtonUp;
+    public Rect ButtonDown;
+
+	// Use this for initialization
+	void Start () {
+        HealthBar = new Rect(400, 40, 200, 20);  //血条区域
+        ButtonUp = new Rect(400, 15, 80, 20);   //加血按钮
+        ButtonDown = new Rect(490, 15, 80, 20); //减血按钮
+        curhealth = health;
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(ButtonUp,"HealthUp"))
+        {
+            curhealth = curhealth + 0.1f > 1.0f ? 1.0f : curhealth + 0.1f;
+        }
+        if (GUI.Button(ButtonDown, "HealthDown"))
+        {
+            curhealth = curhealth - 0.1f < 0.0f ? 0.0f : curhealth - 0.1f;
+        }
+        //插值计算health值，以实现血条值平滑变化
+        health = Mathf.Lerp(health, curhealth, 0.05f);
+        slider.value = health;
+        GUI.HorizontalScrollbar(HealthBar, 0.0f, health, 0.0f, 1.0f);
+    }
+}
+
---
+++ homework9/Assets/Scripts/healthbarUGUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthbarUGUI : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        this.transform.LookAt(Camera.main.transform.position);//面向摄像机
+	}
+}
